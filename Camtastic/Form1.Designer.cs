b2b5eb1f599c0131bfd17eb7e9f5df5000f68dc8
Samuil_D <msamuil@abv.bg>
2022-07-06 19:28:17 +0300

    implemented logic (only the click error to fix)

--- Camtastic/Form1.Designer.cs
+++ Camtastic/Form1.Designer.cs
@@ -30,18 +30,20 @@
         {
             this.linkInput_textBox = new System.Windows.Forms.TextBox();
             this.btn_Search = new System.Windows.Forms.Button();
+            this.exceptionsLabel = new System.Windows.Forms.Label();
+            this.displayBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // linkInput_textBox
             // 
-            this.linkInput_textBox.Location = new System.Drawing.Point(134, 49);
+            this.linkInput_textBox.Location = new System.Drawing.Point(34, 49);
             this.linkInput_textBox.Name = "linkInput_textBox";
             this.linkInput_textBox.Size = new System.Drawing.Size(577, 22);
             this.linkInput_textBox.TabIndex = 0;
             // 
             // btn_Search
             // 
-            this.btn_Search.Location = new System.Drawing.Point(796, 49);
+            this.btn_Search.Location = new System.Drawing.Point(645, 49);
             this.btn_Search.Name = "btn_Search";
             this.btn_Search.Size = new System.Drawing.Size(175, 23);
             this.btn_Search.TabIndex = 1;
@@ -49,12 +51,32 @@
             this.btn_Search.UseVisualStyleBackColor = true;
             this.btn_Search.Click += new System.EventHandler(this.search_ClickEvent);
             // 
+            // exceptionsLabel
+            // 
+            this.exceptionsLabel.AutoSize = true;
+            this.exceptionsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exceptionsLabel.ForeColor = System.Drawing.Color.Red;
+            this.exceptionsLabel.Location = new System.Drawing.Point(33, 85);
+            this.exceptionsLabel.Name = "exceptionsLabel";
+            this.exceptionsLabel.Size = new System.Drawing.Size(0, 29);
+            this.exceptionsLabel.TabIndex = 2;
+            // 
+            // displayBox
+            // 
+            this.displayBox.Location = new System.Drawing.Point(34, 142);
+            this.displayBox.Multiline = true;
+            this.displayBox.Name = "displayBox";
+            this.displayBox.Size = new System.Drawing.Size(786, 279);
+            this.displayBox.TabIndex = 3;
+            // 
             // MainScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleGreen;
-            this.ClientSize = new System.Drawing.Size(1174, 703);
+            this.ClientSize = new System.Drawing.Size(848, 461);
+            this.Controls.Add(this.displayBox);
+            this.Controls.Add(this.exceptionsLabel);
             this.Controls.Add(this.btn_Search);
             this.Controls.Add(this.linkInput_textBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -69,6 +91,8 @@
 
         private System.Windows.Forms.TextBox linkInput_textBox;
         private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.Label exceptionsLabel;
+        private System.Windows.Forms.TextBox displayBox;
     }
 }
 
--- Camtastic/Form1.cs
+++ Camtastic/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,19 +9,112 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Camtastic.Entity;
+using Camtastic.Repository;
 
 namespace Camtastic
 {
     public partial class MainScreen : Form
     {
+        //properties
+        private static IWebDriver driver;
+        private static MainRepository repo;
+
+        //constructor
         public MainScreen()
         {
             InitializeComponent();
+            repo = new MainRepository();
         }
 
         private void search_ClickEvent(object sender, EventArgs e)
         {
+            //extracting the url from the textbox
+            string url = linkInput_textBox.Text;
 
+            //Checks if the url covers the requirements
+            if (!urlIsAvailable(url))
+            {
+                exceptionsLabel.Text = "Invalid URL !!";
+                return;
+            }
+                
+
+            //instancing the driver
+            driver = new ChromeDriver(@"D:\chromedriver2\");
+
+            //navigating to the site
+            driver.Navigate().GoToUrl(url);
+
+            //checks if the url we search have an existing picture
+            try
+            {
+                driver.FindElement(By.XPath("//button[@id='exifInfoMobile']")).Click();
+            }
+            catch (Exception ex)
+            {
+                exceptionsLabel.Text = "The image you search is not found or its deleted !!";
+                return;
+            }
+
+            
+            //extracting the brand and the model out of the html
+            string brand = driver.FindElement(
+                By.XPath("/html/body/div[4]/div[5]/div[1]/div[1]/div/div[2]/div/div[2]/div[1]/div[2]/span")
+                ).Text;
+
+            string model = driver.FindElement(
+                By.XPath("/html/body/div[4]/div[5]/div[1]/div[1]/div/div[2]/div/div[2]/div[2]/div[2]/span")
+                ).Text;
+
+            //checks if theres info on the rows for brand and model
+            if (brand.Equals("") && model.Equals(""))
+            {
+                exceptionsLabel.Text = "The photo you are searching doesnt have information about the camera !!";
+                return;
+            }
+                
+
+            //checking if we have extracted exactly the model and the brand else we return 
+            DateTime date;
+            if (DateTime.TryParse(brand, out date))
+                return;
+
+            //create and fill the camera and photo entity
+            Camera camera = new Camera();
+            camera.brand = brand;
+            camera.model = model;
+
+            //inserting the camera into the database
+            repo.addCamera(camera);
+
+            Photo photo = new Photo();
+            photo.URL = url;
+            photo.rating = int.Parse(
+                driver.FindElement(
+                    By.XPath("/html/body/div[4]/div[5]/div[2]/div/div/div[2]/div/ul[1]/li[1]/ul/li[1]/span[2]/span")
+                    ).Text);
+            photo.cameraID = repo.findCamera(brand, model).ID;
+            photo.camera = repo.findCamera(brand, model);
+
+            //inserting the photo into the database
+            repo.addPhoto(photo);
+
+            //creating and displaying the message to the textbox
+            string message = $"Photo: \n" +
+                                $"URL: {photo.URL}\n Rating: {photo.rating}\\n" +
+                                $"Camera:\n" +
+                                $"Brand: {brand}\n model: {camera.model}";
+            displayBox.Text = message;
+        }
+
+        //checks the url wether its correct
+        public bool urlIsAvailable(string url)
+        {
+            if (url.Contains("https://photo-forum.net/i/"))
+                return true;
+
+            return false;
         }
     }
 }
